449afad21f2c763e9329d4af6e1c49ced96be825
Brian Begy <brian.begy@kazoohr.com>
2021-12-05 13:02:13 -0600

    fix q2 test names

--- src/python/02/question_02_test.py
+++ src/python/02/question_02_test.py
@@ -4,16 +4,16 @@ from question_02 import answer
 
 
 class Question1(unittest.TestCase):
-    def test_question_1a_trivial_input(self):
+    def test_question_2a_trivial_input(self):
         self.assertEqual(answer("src/python/02/sample.txt"), 150)
 
-    def test_question_1b_trivial_input(self):
+    def test_question_2b_trivial_input(self):
         self.assertEqual(answer("src/python/02/sample.txt", True), 900)
 
-    def test_question_1a(self):
+    def test_question_2a(self):
         self.assertEqual(answer("src/python/02/input.txt"), 1989014)
 
-    def test_question_1b(self):
+    def test_question_2b(self):
         self.assertEqual(answer("src/python/02/input.txt", True), 2006917119)
 
 
